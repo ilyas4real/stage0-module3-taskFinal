c3e93e769149153e687c0cb43aea3cead2ba1fa1
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-02 21:20:59 +0100

    completed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result = 0;
+        while (number >= 1) {
+            result = result + number % 10;
+            number = number / 10;
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result = 0;
+        while (number >= 1) {
+            result = result * 10 + number % 10;
+            number = number / 10;
+        }
+        System.out.println(result);
     }
 }
